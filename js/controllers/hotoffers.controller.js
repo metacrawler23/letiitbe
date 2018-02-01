afe301ac92f35f651c74c9c32d7ab40a6307d533
Unknown <cj@sportspass.com.au>
2018-02-01 15:44:29 +0800

    Fix Naming and add clear $scope to  get All Hot Offer

--- js/controllers/hotoffers.controller.js
+++ js/controllers/hotoffers.controller.js
@@ -6,30 +6,32 @@ hotofferCtrl.$inject = ['$scope', 'BannerService', 'CategoryService'];
 function hotofferCtrl($scope, BannerService, CategoryService) {
 
     /**
+     * Get All Banner Hot offers Type
      * @return Banner Hot Offers Type
      */
     $scope.getHotOffers = function() {
-
+      $scope.banners = [];
       BannerService.getBannerHotOffers().then(function(response){
         $scope.banners = response.data;
         console.log(response);
       });
     };
 
-    $scope.getCategories = function() {
-
-      CategoryService.getAll().then(function(response){
-
-        $scope.categories = response.data;
+    /**
+     * Get All Banner Hot offers Type Filter by Catetgory
+     * @return Banner Hot Offers Type Filter by Catetgory
+     */
+    $scope.getHotOffersFilter = function (id) {
+      $scope.banners = [];
+      BannerService.getBannerFilterCategories(id).then(function(response){
+        $scope.banners = response.data.banners;
         console.log(response);
       });
     };
 
-    $scope.getTiles = function (id) {
-      $scope.banners = [];
-
-      BannerService.getBannerFilterCategories(id).then(function(response){
-        $scope.banners = response.data.banners;
+    $scope.getCategories = function() {
+      CategoryService.getAll().then(function(response){
+        $scope.categories = response.data;
         console.log(response);
       });
     };
